e0dada19d959aebf12052faf9ac5947de1b0bc31
Yadwinder Singh <ryker1990@gmail.com>
2016-05-04 16:16:12 +0530

    Update LPS25HB.ino

--- Particle/LPS25HB.ino
+++ Particle/LPS25HB.ino
@@ -4,14 +4,20 @@
 // This code is designed to work with the LPS25HB_I2CS I2C Mini Module available from ControlEverything.com.
 // https://www.controleverything.com/content/Barometer?sku=LPS25HB_I2CS#tabs-0-product_tabset-2
 
-#include<Wire.h>
+#include <application.h>
+#include <spark_wiring_i2c.h>
 
 // LPS25HB I2C address is 0x5C(92)
 #define Addr 0x5C
 
-void setup() 
+float pressure = 0;
+void setup()
 {
-  // Initialise I2C communication as MASTER 
+  // Set variable
+  Particle.variable("i2cdevice", "LPS25HB");
+  Particle.variable("pressure", pressure);
+
+  // Initialise I2C communication as MASTER
   Wire.begin();
   // Initialise Serial communication, set baud rate = 9600
   Serial.begin(9600);
@@ -20,43 +26,40 @@ void setup()
   Wire.beginTransmission(Addr);
   // Select control register 1
   Wire.write(0x20);
-  // Set active mode, continuous Update
+  // Set active mode, continuous update
   Wire.write(0x90);
   // Stop I2C Transmission
   Wire.endTransmission();
-  delay(300); 
+  delay(300);
 }
 
 void loop()
 {
   unsigned int data[3];
+  
   // Start I2C Transmission
   Wire.beginTransmission(Addr);
   // Select pressure data register
   Wire.write(0x28 | 0x80);
   // Stop I2C Transmission
   Wire.endTransmission();
-  
-  // Request 2 bytes of data
+
+  // Request 3 bytes of data
   Wire.requestFrom(Addr, 3);
 
   // Read 3 bytes of data, lsb first
-  if(Wire.available() == 3)
+  if (Wire.available() == 3)
   {
     data[0] = Wire.read();
     data[1] = Wire.read();
     data[2] = Wire.read();
   }
-  delay(300);
-    
+  delay (300);
+
   // Convert pressure data
-  float pressure = ((data[2] * 65536) + (data[1] * 256) + data[0]) / 4096.0;
-  
-  // Output data to serial monitor
-  Serial.print("Pressure is : ");
-  Serial.print(pressure);
-  Serial.println(" hPa"); 
+  pressure = ((data[2] * 65536) + (data[1] * 256) + data[0]) / 4096.0;
+
+  // Output data to dashboard
+  Particle.publish("Pressure :  ", String(pressure));
   delay(1000);
 }
-
-
